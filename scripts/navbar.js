bd7b08a2388e91cf0db1da6b5865589a86816712
Aiffelowy <adas.krzywicki2@gmail.com>
2024-10-06 16:24:03 +0200

    fixed logo positioning

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
     var x = document.getElementById("nav-menu");
+    let logo = document.querySelector("nav img");
     if (x.className === "topnav") {
         x.className += " responsive";
+        logo.hidden = true;
     } else {
         x.className = "topnav";
+        logo.hidden = false;
     }
 }
